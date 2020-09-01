743e28395220aada1e4f149a091c1f6e858444aa
Ahmed-Elawad <a.elawad97@gmail.com>
2020-09-01 14:30:28 -0400

    Video listEntries

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 // TODO: Render the `App` component to the DOM
 import App from './components/App.js';
+import VideoList from './components/videoList.js';
+import exampleVideoData from './data/exampleVideoData.js';
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<VideoList videos={exampleVideoData} />, document.getElementsByClassName('col-md-5')[0]);
+// commnet added
 
