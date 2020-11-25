428da96718d1dcd76b41c47cff60be99b56969e6
Chris Cali <ChristopherCali1415@gmail.com>
2020-11-25 18:03:21 -0500

    Refactoring for better readability

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,22 +1,17 @@
 
 import VideoListEntry from './VideoListEntry.js';
 
-
-var VideoList = (prop) => (
-  <div className="video-list" onClick={prop.onClick} >
-    {prop.videos.map( (video) => (
-      <VideoListEntry video={video}/>
+const VideoList = ({videos, onClick }) => (
+  <div className="video-list" onClick={() => { onClick(); }} >
+    {videos.map( (video) => (
+      <VideoListEntry key={video.id.videoId} video={video}/>
     ))}
   </div>
 );
 
-// PropTypes tell other developers what `props` a component expects
-// Warnings will be shown in the console when the defined rules are violated
+// Tells other developers what `props` a component expects
 VideoList.propTypes = {
   videos: PropTypes.array.isRequired
 };
 
-// In the ES6 spec, files are "modules" and do not share a top-level scope.
-// `var` declarations will only exist globally where explicitly defined.
-// ReactDOM.render(<VideoList videos={exampleVideoData}/>, document.getElementsByClassName('col-md-5'));
 export default VideoList;
