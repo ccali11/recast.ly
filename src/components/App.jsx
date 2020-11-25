9870a466efc3fa50d1d8c0232b46c7c58d49a2bc
Chris Cali <ChristopherCali1415@gmail.com>
2020-11-25 18:33:16 -0500

    More refactoring to remove unnecessary text

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -38,10 +38,10 @@ class App extends React.Component {
       </nav>
       <div className="row">
         <div className="col-md-7">
-          <div><h5><em>videoPlayer</em> <VideoPlayer video={this.state.playing} /></h5></div>
+          <div><h5> <VideoPlayer video={this.state.playing} /></h5></div>
         </div>
         <div className="col-md-5">
-          <div onClick={this.onVideoClick.bind(this)}><h5><em>videoList</em><VideoList videos={exampleVideoData}/></h5></div>
+          <div onClick={this.onVideoClick.bind(this)}><h5><VideoList videos={exampleVideoData}/></h5></div>
         </div>
       </div>
     </div>
